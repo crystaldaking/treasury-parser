0eaee678acff1c37f3bd1bb8c31f91b5eb98c01e
Pavel Bihel <crystal@crystalapps.by>
2023-09-15 13:38:57 +0200

    WIP: define routes

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"github.com/gofiber/fiber/v2"
+	"log"
+)
 
+func main() {
+	app := fiber.New()
+
+	app.Post("/update", func(ctx *fiber.Ctx) error {
+		return ctx.SendString("implement update route")
+	})
+
+	app.Get("/state", func(ctx *fiber.Ctx) error {
+		return ctx.SendString("implement state route")
+	})
+
+	app.Get("/get_names", func(ctx *fiber.Ctx) error {
+		return ctx.SendString("implement get_names route")
+	})
+
+	log.Fatal(app.Listen(":8080"))
 }
